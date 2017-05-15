97a2d18cda277a3c0eb8853dbc5b5e8f9ffa482f
Nikhil Punwaney <nikhilnp@mit.edu>
2017-05-15 12:19:03 -0400

    more data

--- parseMFCC.py
+++ parseMFCC.py
@@ -1,14 +1,38 @@
 import numpy as np
 from collections import Counter
 
-XTrain = np.load('./Data/X_Train.npy')
-YTrain = np.load('./Data/Y_Train.npy')
+# XTrain = np.load('./Data/X_Train.npy')
+# YTrain = np.load('./Data/Y_Train.npy')
 
-XTest = np.load('./Data/X_Test.npy')
-YTest = np.load('./Data/Y_Test.npy')
+# XTest = np.load('./Data/X_Test.npy')
+# YTest = np.load('./Data/Y_Test.npy')
 
-def getDifference(array1, array2):
-	return np.subtract(array1[0:5], array2[0:5])
+# XTrain = np.load('./Data/X_Train_Middle.npy')
+# YTrain = np.load('./Data/Y_Train_Middle.npy')
+
+# XTest = np.load('./Data/X_Test_Middle.npy')
+# YTest = np.load('./Data/Y_Test_Middle.npy')
+
+# XTrain = np.load('./Data/X_Train_WithCov.npy')
+# YTrain = np.load('./Data/Y_Train_WithCov.npy')
+
+# XTest = np.load('./Data/X_Test_WithCov.npy')
+# YTest = np.load('./Data/Y_Test_WithCov.npy')
+
+# XTrain = np.load('./Data/X_Train_WithReggae.npy')
+# YTrain = np.load('./Data/Y_Train_WithReggae.npy')
+
+# XTest = np.load('./Data/X_Test_WithReggae.npy')
+# YTest = np.load('./Data/Y_Test_WithReggae.npy')
+
+# X = np.load('./Data/X.npy')
+X_first = np.load('./Data/X_first.npy')
+X_middle = np.load('./Data/X_middle.npy')
+X_end = np.load('./Data/X_end.npy')
+Y = np.load('./Data/Y.npy')
+
+def getDifference(array1, array2, numFeatures):
+	return np.subtract(array1[0:numFeatures], array2[0:numFeatures])
 
 def squareArray(array, power):
 	return np.power(array, power)
@@ -32,16 +56,18 @@ def makeArrayTuples(keys, labels):
 		key = keys[k]
 		label = labels[k]
 		genre = arrayGenre(label)
+		# if genre != "Jazz":
 		tuples.append((key, genre))
 	return tuples
 
-def getClosestArrays(array, otherArrays, k):
+def getClosestArrays(array, otherArrays, k, numFeatures):
 	tuples = []
 	for a in otherArrays:
-		diff = getDifference(array, a[0])
-		square = squareArray(diff, 2)
-		squareSum = sumOfArray(square)
-		tuples.append((a[0], a[1], squareSum))
+		if not np.array_equal(array, a[0]):
+			diff = getDifference(array, a[0], numFeatures)
+			square = squareArray(diff, 2)
+			squareSum = sumOfArray(square)
+			tuples.append((a[0], a[1], squareSum))
 	sortedTuples = sorted(tuples, key = lambda x: x[2])
 	return sortedTuples[:k]
 
@@ -50,20 +76,99 @@ def getModeOfTuples(tuples):
 	data = Counter(groups)
 	return data.most_common(3)
 
-def getCorrect(xTrain, yTrain, xTest, yTest, k):
+def printDataInfo(data):
+	for key in data.keys():
+		value = data[key]
+		percentDict = {}
+		total = sum(value.values())
+		for k in value.keys():
+			v = value[k]
+			percentDict[k] = 100.0 * v / float(total)
+		print key
+		print str(data[key])
+		print str(percentDict)
+		print
+
+def getCorrect(xTrain, yTrain, xTest, yTest, k, numFeatures):
+	data = {}
 	trainArray = makeArrayTuples(xTrain, yTrain)
 	testArray = makeArrayTuples(xTest, yTest)
 	correctCount = 0
 	for a in testArray:
-		closestArrays = getClosestArrays(a[0], trainArray, k)
+		actual = a[1]
+		closestArrays = getClosestArrays(a[0], trainArray, k, numFeatures)
 		mode = getModeOfTuples(closestArrays)
-		if mode[0][0] == a[1]:
+		expected = mode[0][0]
+		if expected == actual:
 			correctCount += 1
-	return correctCount/float(len(yTest))
+		dataValue = data.get(actual, {})
+		dataValue[expected] = dataValue.get(expected, 0) + 1
+		data[actual] = dataValue
+	return 100 * correctCount/float(len(testArray))
+	# printDataInfo(data)
+	# print 100 * correctCount/float(len(testArray))
 
-for i in range(1, 20):
-	print getCorrect(XTrain, YTrain, XTest, YTest, i)
+def getCorrect_Full(x_first, x_middle, x_end, y, k, numFeatures):
+	data = {}
+	firstArray = makeArrayTuples(x_first, y)
+	middleArray = makeArrayTuples(x_middle, y)
+	endArray = makeArrayTuples(x_end, y)
+	correctCount = 0
+	for i in range(len(firstArray)):
+		first = firstArray[i]
+		middle = middleArray[i]
+		end = endArray[i]
+		actual = first[1]
+		closestArrays = getClosestArrays(first[0], firstArray, k, numFeatures) + getClosestArrays(middle[0], middleArray, k, numFeatures) + getClosestArrays(end[0], endArray, k, numFeatures)
+		mode = getModeOfTuples(closestArrays)
+		expected = mode[0][0]
+		if expected == actual:
+			correctCount += 1
+		dataValue = data.get(actual, {})
+		dataValue[expected] = dataValue.get(expected, 0) + 1
+		data[actual] = dataValue
+	return 100 * correctCount/float(len(firstArray))
+	# printDataInfo(data)
+	# print 100 * correctCount/float(len(testArray))
 
+# Start
+# k = 14, numFeatures = 7, 47.5
+# Without Jazz
+# k = 10, numberFeatures = 12
+
+# Middle
+# k = 19, numberFeatures = 3
+# Without Jazz
+# k = 17, numberFeatures = 5
+
+# WithCov
+# k = 7, numberFeatures = 68
+# Without Jazz
+# k = 3, numberFeatures = 95
+
+# WithReggae
+# k = 3, numberFeatures = 96
+
+# Knife Method
+# k = 8, numberFeatures = 34
+
+# getCorrect(XTrain, YTrain, XTest, YTest, 3, 96)
+# getCorrect(X, Y, X, Y, 8, 34)
+
+bestValue = 0
+bestIndex = (None, None)
+
+for i in range(1, 10):
+	print "K = " + str(i)
+	for j in range(3, 98):
+		value = getCorrect(X_first, X_middle, X_end, Y, i, j)
+		if value > bestValue:
+			bestValue = value
+			print bestValue
+			bestIndex = (i, j)
+
+print "Best Accuracy: " + str(bestValue)
+print "Best Index: " + str(bestIndex)
 
 
 
